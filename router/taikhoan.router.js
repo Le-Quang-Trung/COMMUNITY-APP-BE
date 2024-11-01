b883a2d40269453f081be69956cb3dcf4aa68784
TRUNG <leqtrungggg3105@gmail.com>
2024-11-02 01:08:37 +0700

    API LoginSV & API LoginGV

--- router/taikhoan.router.js
+++ router/taikhoan.router.js
@@ -18,7 +18,30 @@ routerTaiKhoan.get('/', (req, res, next) => {
         })
 })
 
-routerTaiKhoan.post('/login', async (req,res,next) => {
+routerTaiKhoan.post('/loginsv', async (req,res,next) => {
+    try{
+        const {tenTaiKhoan, matKhau} = req.body;
+        const taikhoan = await TaiKhoanModel.findOne({tenTaiKhoan: tenTaiKhoan});
+        if (!taikhoan) {
+            return res.status(405).json({"Can't find user with this tenTaiKhoan": tenTaiKhoan});
+        }
+        if (taikhoan.matKhau !== matKhau) {
+            return res.status(404).json({ "Password is incorrect": matKhau });
+        }
+        if (taikhoan.quyen !== 'SV') {
+            return res.status(403).json({ "Không đủ quyền hạn truy cập": true });
+        }
+        delete taikhoan.matKhau;
+        res.status(200).json(taikhoan);
+    }
+    catch (err){
+        console.error(err.message);
+        res.status(500).json({message: err.message});
+    }
+})
+
+routerTaiKhoan.post('/logingv', async (req,res,next) => {
+  
     try{
         const {tenTaiKhoan, matKhau} = req.body;
         const taikhoan = await TaiKhoanModel.findOne({tenTaiKhoan: tenTaiKhoan});
@@ -28,6 +51,9 @@ routerTaiKhoan.post('/login', async (req,res,next) => {
         const isMatKhauValid = await bcrypt.compare(matKhau, taikhoan.matKhau);
         if (!isMatKhauValid) {
             return res.status(404).json({"Password is incorrect": matKhau});
+        }
+        if (taikhoan.quyen !== 'GV') {
+            return res.status(403).json({ "Không đủ quyền hạn truy cập": true });
         }
         delete taikhoan.matKhau;
         res.status(200).json(taikhoan);
